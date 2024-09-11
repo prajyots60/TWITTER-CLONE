4c73bb3e2519fcaf4805330e292b10b05749a99c
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-09-11 17:38:59 +0530

    04-Refactor server.js and package.json to support production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import {v2 as cloudinary} from 'cloudinary';
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '5mb'}));
 
@@ -32,6 +34,14 @@ app.use("/api/auth" , authRoutes);
 app.use("/api/users" , userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT , () => {
   console.log(`server started at port ${PORT}`);
