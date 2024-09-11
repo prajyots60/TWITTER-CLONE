c94c97f4b0879322314b2476b533a83eb016b637
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-09-11 23:57:41 +0530

    05-my commits

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({limit: '5mb'}));
